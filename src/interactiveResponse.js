c197a73a48e03dc55caeeb5305a21ae231cb9299
Andrew Mahoney-Fernandes <andrew.mahoney.f@gmail.com>
2018-07-22 19:32:04 -0400

    Add files via upload

---
+++ src/interactiveResponse.js
@@ -0,0 +1,218 @@
+module.exports =  {
+
+    getFrequencyMessage() {
+        return {
+            'text': 'Lets schedule a facilitator!',
+            'attachments': [
+                {
+                    'text': 'How frequent do you want to me to choose someone?',
+                    'fallback': 'You are unable to schedule a facilitator',
+                    'callback_id': 'randy_frequency',
+                    'color': '#3AA3E3',
+                    'attachment_type': 'default',
+                    'actions': [
+                        {
+                            'name': 'frequency',
+                            'text': '1 minute',
+                            'type': 'button',
+                            'value': 'one-minute'
+                        },
+                        {
+                            'name': 'frequency',
+                            'text': 'Daily',
+                            'type': 'button',
+                            'value': 'daily'
+                        },
+
+                        {
+                            'name': 'frequency',
+                            'text': 'Work Days',
+                            'type': 'button',
+                            'value': 'work_days'
+                        },
+                        {
+                            'name': 'frequency',
+                            'text': 'Weekly',
+                            'type': 'button',
+                            'value': 'weekly'
+                        },
+                                        {
+                            'name': 'frequency',
+                            'text': 'Cancel',
+                            'type': 'button',
+                            'value': 'cancel',
+                            'style': 'danger'
+                        }
+                    ]
+                }
+            ]
+        };
+    },
+
+    getDailyHourResponse() {
+        return {
+            'text': 'Lets get some frequency details',
+            'attachments': [
+                {
+                    'text': 'What time should I select a facilitator?',
+                    'fallback': 'You are unable to schedule a facilitator',
+                    'callback_id': 'randy_details_hour',
+                    'color': '#3AA3E3',
+                    'attachment_type': 'default',
+                    'actions': [
+                        {
+                            "name": "hour_list",
+                            "text": "Pick an hour...",
+                            "type": "select",
+                            "options": [
+                                {
+                                    "text": "1",
+                                    "value": "1"
+                                },
+                                {
+                                    "text": "2",
+                                    "value": "2"
+                                },
+                                {
+                                    "text": "3",
+                                    "value": "3"
+                                },
+                                {
+                                    "text": "4",
+                                    "value": "4"
+                                },
+                                {
+                                    "text": "5",
+                                    "value": "5"
+                                },
+                                {
+                                    "text": "6",
+                                    "value": "6"
+                                },
+                                {
+                                    "text": "7",
+                                    "value": "7"
+                                },
+                                {
+                                    "text": "8",
+                                    "value": "8"
+                                },
+                                {
+                                    "text": "9",
+                                    "value": "9"
+                                },
+                                {
+                                    "text": "10",
+                                    "value": "10"
+                                },
+                                {
+                                    "text": "11",
+                                    "value": "11"
+                                },
+                                {
+                                    "text": "12",
+                                    "value": "12"
+                                }
+                            ]
+                        }
+                    ]
+                }
+            ]
+        }
+    },
+
+    getDailyMinuteResponse() {
+        return {
+            'text': 'Lets get some frequency details',
+            'attachments': [
+                {
+                    'text': 'What time should I select a facilitator?',
+                    'fallback': 'You are unable to schedule a facilitator',
+                    'callback_id': 'randy_details_minute',
+                    'color': '#3AA3E3',
+                    'attachment_type': 'default',
+                    'actions': [
+                        {
+                            "name": "minute_list",
+                            "text": "Pick a minute...",
+                            "type": "select",
+                            "options": [
+                                {
+                                    "text": "00",
+                                    "value": "00"
+                                },
+                                {
+                                    "text": "15",
+                                    "value": "15"
+                                },
+                                {
+                                    "text": "30",
+                                    "value": "30"
+                                },
+                                {
+                                    "text": "45",
+                                    "value": "45"
+                                }
+                            ]
+                        }
+                    ]
+                }
+            ]
+        }
+    },
+
+    getAmPmFrequency() {
+        return {
+            'text': 'Lets get some frequency details',
+            'attachments': [
+                {
+                    'text': 'What time should I select a facilitator?',
+                    'fallback': 'You are unable to schedule a facilitator',
+                    'callback_id': 'randy_details_ampm',
+                    'color': '#3AA3E3',
+                    'attachment_type': 'default',
+                    'actions': [
+                        {
+                            "name": "am_pm_list",
+                            "text": "Night or day...",
+                            "type": "select",
+                            "options": [
+                                {
+                                    "text": "AM",
+                                    "value": "am"
+                                },
+                                {
+                                    "text": "PM",
+                                    "value": "pm"
+                                }
+                            ]
+                        }
+                    ]
+                }
+            ]
+        }
+    },
+
+    getUserPickedMessage(user) {
+        return {
+            'text': `${user} has been picked!`,
+            'attachments': [
+                {
+                    'text': `Would you like to skip ${user}?`,
+                    'fallback': '',
+                    'callback_id': 'randy_next_user',
+                    'color': '#3AA3E3',
+                    'attachment_type': 'default',
+                    'actions': [
+                        {
+                            'name': 'next',
+                            'text': 'Next',
+                            'type': 'button',
+                            'value': 'next'
+                        }
+                    ]
+                }
+            ]
+        }
+    }
+}
---
+++ src/server.js
@@ -0,0 +1,160 @@
+require('dotenv').config();
+const express = require('express');
+const bodyParser = require('body-parser');
+const interactiveResponse = require('./interactiveResponse');
+const mongoose = require('mongoose');
+const moment = require('moment');
+var request = require('request');
+require('./model/SchedulingSession');
+require('./model/Schedule');
+
+const SchedulingSession = mongoose.model('SchedulingSession');
+const Schedule = mongoose.model('Schedule');
+const mongoose_id = process.env.MONGOOSE_USER_ID || '';
+const mongoose_password = process.env.MONGOOSE_PASSWORD || '';
+const mongoose_url = process.env.MONGOOSE_URL;
+const verification_token = process.env.VERIFICATION_TOKEN || '';
+const oauth_token = process.env.OAUTH_TOKEN || '';
+const timer_interval = process.env.TIMER_INTERVAL || 60000;
+
+const app = express();
+const port = process.env.PORT || 3005;
+
+mongoose.connect(`mongodb://${mongoose_id}:${mongoose_password}@${mongoose_url}`, (err, db) => {
+    if (err !== null) {
+        console.log(err)
+    } else {
+        _init();
+    }
+});
+
+const API = '/slack';
+
+const CALLBACK_ID = {
+    FREQUENCY: 'randy_frequency',
+    HOUR: 'randy_details_hour',
+    MINUTE: 'randy_details_minute',
+    AM_PM: 'randy_details_ampm'
+}
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get(API, (req, res) => res.status(200).send('Hello slack!'));
+
+app.post(`${API}/random-facilitator`, (req, res) => {
+    res.set('Content-Type', 'application/json');
+    res.status(200).send(interactiveResponse.getFrequencyMessage());
+});
+
+app.post(`${API}/frequency`, (req, res) => {
+    const payload = JSON.parse(req.body.payload);
+    const callback_id = payload.callback_id;
+    const channel_id = payload.channel.id;
+    const user_id = payload.user.id;
+
+    switch(callback_id) {
+        case 'randy_frequency': _saveScheduling(user_id, channel_id, 'frequency', payload.actions[0].value);
+    }
+
+    if (callback_id === CALLBACK_ID.FREQUENCY) {
+        if (payload.actions[0].value === 'one-minute'){
+            _setupSchedule(user_id, channel_id, payload.actions[0].value);
+            res.status(200).send('I will now randomly select a user every minute.');
+        } else if (payload.actions[0].value === 'daily') {
+            res.status(200).send(interactiveResponse.getDailyHourResponse());
+        }
+    } else {
+        res.status(200);
+    }
+});
+
+app.use((err, req, res, next) => {
+    console.log(err.stack);
+    res.status(400).send(err.message);
+});
+
+app.listen(port, () => console.log(`bot listening on port ${port}`));
+
+function sendNewSelectionToChannel(channel_id) {
+    request.get(`https://slack.com/api/channels.info?token=${verification_token}&channel=${channel_id}`, (error, response, body) => {
+        // console.log(`channel info: ${body}`);
+        const members = JSON.parse(body).channel.members;
+        const randomUser = members[Math.floor(Math.random() * members.length)];
+
+        request.get(`https://slack.com/api/users.info?token=${verification_token}&user=${randomUser}`, (error, response, body) => {
+            // console.log(`channel: ${channel_id}`);
+            // console.log(JSON.parse(body));
+            const name = JSON.parse(body).user.real_name;
+            const msg = interactiveResponse.getUserPickedMessage(name);
+            // const options = {
+            //     url: `https://slack.com/api/chat.postMessage?token=${oauth_token}&text=${name}%20has%20been%20picked&channel=${channel_id}`,
+            //     method: 'POST',
+            //     'Content-Type': 'application/json'
+            // }
+            request(`https://slack.com/api/chat.postMessage?token=${oauth_token}&text=${name}%20has%20been%20picked&channel=${channel_id}`, (error, response, body) => {
+                console.log(`Error sending post message: error:${error} response:${JSON.stringify(response)} body:${JSON.stringify(body)}`)
+            })
+            // const options = {
+            //     url: 'https://slack.com/api/chat.postMessage',
+            //     form: {
+            //         token
+            //     }
+            // }
+            // request.post(options, (error, response, body) => {
+
+            // });
+        })
+    })
+}
+
+function _saveScheduling(user_id, channel_id, type, typeData) {
+    SchedulingSession.findOne({'channel_id': channel_id, 'user_id': user_id}, (err, data) => {
+        let newData = data == null ? new SchedulingSession() : data;
+        newData.user_id = user_id;
+        newData.channel_id = channel_id;
+        newData[type] = typeData;
+        newData.save().then((err) => {
+            if (err != null) {
+                console.log(err);
+            }
+        });
+    });
+}
+
+function _setupSchedule(user_id, channel_id, type) {
+    SchedulingSession.findOne({'channel_id': channel_id, 'user_id': user_id}, (err, data) => {
+        if (data) {
+            console.log(type);
+            switch(type) {
+                case 'one-minute':
+                    const currentTime = moment().add(1, 'm');
+                    console.log(currentTime.utc().format('YYYYMMDDHHmm'));
+                    let saveDate = new Schedule({
+                        alert_time: currentTime.utc().format('YYYYMMDDHHmm'),
+                        channel_id: channel_id
+                    });
+                    saveDate.save()
+            }
+        }
+    });
+}
+
+function _init() {
+    _start();
+    setInterval(_start, timer_interval)
+}
+
+function _start(){
+    const currentTime = moment().utc().format('YYYYMMDDHHmm');
+    console.log(`retrieving schedules for ${currentTime}`);
+    Schedule.find({alert_time: currentTime}, (err, data) => {
+        if(err) {
+            console.log(err);
+        } else {
+            data.forEach(item => {
+                sendNewSelectionToChannel(item.channel_id);
+            })
+        }
+    });
+}
